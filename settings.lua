8ce32760c643da3194a502b01f824baaefcaae9e
DinoStduio <122707493+DinoStduio@users.noreply.github.com>
2023-01-15 14:05:42 +0800

    Create settings.lua

---
+++ settings.lua
@@ -0,0 +1,8 @@
+getgenv().Settings = {
+    ["Enabled"] = true, -- Set this to false when you want to disable the bots
+    ["Message"] = "making your avatar in disk cod (cupcakes 69 15)", -- Replace with the message you want the bots to say
+    ["Type"] = "Whisper", -- Types - Whisper/All. Whisper will /w everyone then hop, all with spam the message a determined amount of times. Whisper is recommended
+    ["Messages"] = 10, -- If type is set to all, otherwise it'll just whisper everyone
+    ["Cooldown"] = 5, -- Cooldown between messages, 2.5 is the minimum you can do without roblox timing you out
+    ["ServerHop"] = true -- true = it will server hop, false means it won't
+}
